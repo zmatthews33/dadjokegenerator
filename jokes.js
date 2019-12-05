4e5acc24ce6a40a14cca233524bccc9ae0437fb6
zmatthews33 <zmatt33@gmail.com>
2019-12-05 14:14:57 -0600

    API fetch to icanhazdadjokes API

--- jokes.js
+++ jokes.js
@@ -1,3 +1,8 @@
+const jokeButton = document.querySelector
+('.getJoke');
+const jokeHolder = document.querySelector
+('joke p');
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +13,17 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+async function fetchJoke() {
+  // turn loader on
+  loader.classList.remove('hidden');
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  // turn the loader off
+  loader.classList.add('hidden');
+  return data;
+}
+
